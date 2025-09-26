c219469df8538d046014991b01216dc5a8175b21
mars22144 <fauziadnansptr@gmail.com>
2025-09-26 11:02:04 +0700

    fixing logic

--- tebak angka/script.js
+++ tebak angka/script.js
@@ -5,6 +5,31 @@ while (ask) {
     let tebakanBenar = false
     let player
 
+    //mendefinisikan angka 1 - 10
+    let computer = Math.random()
+
+    if (computer <= 0.1) {
+        computer = "1"
+    } else if (computer <= 0.2) {
+        computer = "2"
+    } else if (computer <= 0.3) {
+        computer = "3"
+    } else if (computer <= 0.4) {
+        computer = "4"
+    } else if (computer < 0.5) {
+        computer = "5"
+    } else if (computer <= 0.6) {
+        computer = "6"
+    } else if (computer <= 0.7) {
+        computer = "7"
+    } else if (computer <= 0.8) {
+        computer = "8"
+    } else if (computer <= 0.9) {
+        computer = "9"
+    } else {
+        computer = "10"
+    }
+
     while (change > 0 && !tebakanBenar) {
         player = prompt(`masukkan angka 1 - 10 \nAnda memiliki ${change} kesempatan`)
 
@@ -14,53 +39,25 @@ while (ask) {
             break
         }
 
-        //mendefinisikan angka 1 - 10
-        let computer = Math.random()
-
-        if (computer <= 0.1) {
-            computer = "1"
-        } else if (computer <= 0.2) {
-            computer = "2"
-        } else if (computer <= 0.3) {
-            computer = "3"
-        } else if (computer <= 0.4) {
-            computer = "4"
-        } else if (computer < 0.5) {
-            computer = "5"
-        } else if (computer <= 0.6) {
-            computer = "6"
-        } else if (computer <= 0.7) {
-            computer = "7"
-        } else if (computer <= 0.8) {
-            computer = "8"
-        } else if (computer <= 0.9) {
-            computer = "9"
-        } else {
-            computer = "10"
-        }
-
         //medefinisikan nilai dari hasil
-        let hasil = ""
-
         if (computer === player) {
             tebakanBenar = true
-            hasil = "benar"
+            alert(`Anda memasukkan angka ${player} dan komputer memasukkan angka ${computer}, jawaban anda Benar`)
         } else {
             change--
-            hasil = "salah"
+            let hint = ""
+            if(player > computer){
+                hint = "tertlalu besar"
+            }else{
+                hint = "terlalu kecil"
+            }
+            alert(`Anda memasukkan angka ${player}, jawaban anda Salah ${hint}, Anda memiliki ${change} kesempatan`)
         }
 
-        //pesan terlalu tinggi atau terlalu rendah
-        if(computer > player){
-            alert(`Anda memasukkan angka ${player}, terlalu rendah\nJawaban anda adalah: ${hasil}, Anda memiliki ${change} kesempatan`)
-        }else{
-            alert(`Anda memasukkan angka ${player}, terlalu tinggi\nJawaban anda adalah: ${hasil}, Anda memiliki ${change} kesempatan`)
-        }
-
-        //jika kesempatan sudah habis
-        if(!tebakanBenar && change === 0){
-            alert("Yah, kesempatan anda sudah habis")
-        }
+    }
+    //jika kesempatan sudah habis
+    if(change === 0){
+        alert("Yah, kesempatan anda sudah habis")
     }
     ask = confirm("mau main lagi?")
 }
